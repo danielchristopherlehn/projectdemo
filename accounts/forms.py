a3d864da1b893829422f1340e210109f6f582807
danielchristopherlehn <danielchristopherlehn@gmail.com>
2026-06-05 08:36:14 +0200

    Fixed email issue in user registration

--- accounts/forms.py
+++ accounts/forms.py
@@ -23,11 +23,21 @@ class RegisterForm(UserCreationForm):
             "password2",
         )
 
+    def clean_email(self):
+        email = self.cleaned_data.get("email", "").strip().lower()
+
+        if User.objects.filter(email__iexact=email).exists():
+            raise forms.ValidationError("This email is already registered.")
+
+        return email
+
     def save(self, commit=True):
         user = super().save(commit=False)
         user.email = self.cleaned_data["email"]
         user.first_name = self.cleaned_data["first_name"]
         user.last_name = self.cleaned_data["last_name"]
+
         if commit:
             user.save()
+
         return user
